a36b4d223e23e0855014997ebffab578a61c488d
Winson404 <sonerwin8@gmail.com>
2023-10-20 22:56:54 +0800

    done

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php 
-	session_start();
+session_start();
+	// DATABASE CONFIGURATION ON LIVE SERVER
+	// $conn = mysqli_connect("localhost","facultye_faculty_evaluation","@Saving09509972084","facultye_faculty_evaluation");
 	$conn = mysqli_connect("localhost","root","","faculty_evaluation");
 	if(!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
